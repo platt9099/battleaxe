fa8d4c4a8b6edd4fc7742c688e79530259dca79b
codenation <codenation@codenations-MacBook-Pro.local>
2019-05-23 15:48:36 +0100

    dotnotation.js

--- dotnotation.js
+++ dotnotation.js
@@ -1,4 +1,12 @@
 //comment
-console.log(Math.random()*10);
-console.log(Math.floor(Math.random()*10));`
+console.log ("Hello");
 
+console.log("what data type am I?");
+
+console.log("Hello".lenght);
+
+console.log ( Math.random());
+
+console.log(Math.random()*10);
+
+console.log(Math.floor(Math.random()*10));
